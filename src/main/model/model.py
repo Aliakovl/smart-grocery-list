db28913f130975914e58cdd35ca96f7e7151d415
Aleksei Iakovlev <mister.yakowlev2011@gmail.com>
2021-11-30 19:40:24 +0300

    first commit

---
+++ src/main/model/model.py
@@ -0,0 +1,124 @@
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
+from typing import List
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class Product:
+    name: str
+    quantity: int
+    units: str
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class Recipe:
+    name: str
+    products: List[Product]
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class AvailableProducts:
+    available_products: List[Product]
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class DayPlan:
+    date: str
+    day: str
+    recipes: List[Recipe]
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class User:
+    id: int
+    plan: List[DayPlan]
+    available_products: List[Product]
+
+    def show_grocery_list(self):
+        dct = {}
+        for day_plan in self.plan:
+            for recipe in day_plan.recipes:
+                for product in recipe.products:
+                    if product.name in dct:
+                        dct[product.name] += product.quantity
+                    else:
+                        dct[product.name] = product.quantity
+
+        for product in self.available_products:
+            if product.name in dct:
+                dct[product.name] -= product.quantity
+        return dct
+
+
+if __name__ == '__main__':
+    data = '''{
+  "id": 83936171,
+  "plan": [
+    {
+      "date": "03-12-2021",
+      "day": "Friday",
+      "recipes": [
+        {
+          "name": "Lasagna",
+          "url": "weefwef2",
+          "products": [
+            {
+              "name": "cheese",
+              "quantity": 100,
+              "units": "g"
+            },
+            {
+              "name": "milk",
+              "quantity": 1,
+              "units": "l"
+            }
+          ]
+        },
+        {
+          "name": "Meat balls",
+          "url": "weefwef1",
+          "products": [
+            {
+              "name": "meat",
+              "quantity": 1,
+              "units": "kg"
+            },
+            {
+              "name": "riсe",
+              "quantity": 0.5,
+              "units": "kg"
+            },
+            {
+              "name": "cheese",
+              "quantity": 200,
+              "units": "g"
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "available_products": [
+    {
+      "name": "cheese",
+      "quantity": 50,
+      "units": "g"
+    },
+    {
+      "name": "bread",
+      "quantity": 1,
+      "units": "kg"
+    }
+  ]
+}'''.strip()
+
+    user = User.from_json(data)
+
+    print(user.show_grocery_list())
+
+    # print(user.to_json(indent=4))
---
+++ src/main/tgbot/tgbot.py
@@ -0,0 +1,36 @@
+from telebot import types, TeleBot
+from os import getenv
+
+TOKEN = getenv('TELEGRAM_TOKEN')
+bot = TeleBot(token=TOKEN)
+
+
+markup = types.ReplyKeyboardMarkup(True, False)
+add_recipe_button = types.KeyboardButton('Добавить рецепт')
+add_item_button = types.KeyboardButton('Добавить продукт')
+markup.row(add_recipe_button)
+markup.row(add_item_button)
+
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    bot.send_message(message.chat.id, "Выбери действие", reply_markup=markup)
+
+
+@bot.message_handler(regexp=r'(.*)')
+def start_command(message):
+    print(message)
+    bot.send_message(message.chat.id, "Hello!")
+    print(bot.get_me())
+
+
+@bot.message_handler(content_types=['photo'])
+def start_command(message):
+    file_id = message.json['photo'][-1]['file_id']
+    print(bot.get_file(file_id))
+    print(bot.get_file_url(file_id))
+    print(file_id)
+
+
+if __name__ == '__main__':
+    bot.polling()
